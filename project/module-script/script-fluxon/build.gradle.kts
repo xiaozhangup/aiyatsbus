6577ec81fc4025aa39fa34baea71491a674c2b0d
Micalhl <m@taboo.sh>
2026-03-05 19:17:49 +0800

    升级 fluxon

--- project/module-script/script-fluxon/build.gradle.kts
+++ project/module-script/script-fluxon/build.gradle.kts
@@ -6,10 +6,10 @@ dependencies {
     // 引入 API
     compileOnly(project(":project:common"))
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-    compileOnly("org.tabooproject.fluxon:core:1.6.12")
-    compileOnly("org.tabooproject.fluxon.plugin:core:1.1.0")
-    compileOnly("org.tabooproject.fluxon.plugin:common:1.1.0")
-    compileOnly("org.tabooproject.fluxon.plugin:platform-bukkit:1.1.0")
+    compileOnly("org.tabooproject.fluxon:core:1.6.13")
+    compileOnly("org.tabooproject.fluxon.plugin:core:1.1.1")
+    compileOnly("org.tabooproject.fluxon.plugin:common:1.1.1")
+    compileOnly("org.tabooproject.fluxon.plugin:platform-bukkit:1.1.1")
     // Reflex Remapper
     compileOnly("org.ow2.asm:asm:9.8")
     compileOnly("org.ow2.asm:asm-util:9.8")
